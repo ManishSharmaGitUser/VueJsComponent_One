eb62c759b15a3acb5ff27d9cb1f1044737de440e
manish <manish.7151.sharma@gmail.com>
2021-10-25 02:41:46 -0700

    Component within Component

---
+++ VueJsComponent_One/myjs/tasklist_js.js
@@ -0,0 +1,22 @@
+﻿Vue.component('task-list', {
+    template: '<div><task v-for="item in tasks">{{item.task}}</task></div>',
+    data() {
+        return {
+            tasks: [
+                { task: 'go to store', isdone: true },
+                { task: 'buy fruits', isdone: false },
+                { task: 'buy vegetables', isdone: true }
+
+            ]
+        };
+    }
+});
+
+
+Vue.component('task', {
+template : '<li><slot></slot></li>'
+});
+
+new Vue({
+ el : '#root'
+})
